9d590a30309e012737aa69960ab560b638d86b18
Laptop <janiswelsch@gmail.com>
2025-08-04 19:45:38 +0200

    commit Laptop

---
+++ src/Uebung_02/Anwendung.java
@@ -0,0 +1,26 @@
+/*  
+  Dateiname     : Anwendung.java.
+  Veranstaltung : Anwendungsprogrammierung (BIS-201).
+  Fragestellung : Rahmen Anwendungsklasse.
+  Version       : 14.10.2024
+*/
+//Bitte entsprechend anpassen:
+package Uebung_02;
+
+public class Anwendung {
+  
+  public static void main(String [] args)
+  {
+
+  }
+  
+}
+
+
+
+
+
+
+
+
+
---
+++ src/Uebung_02/Warenautomat.java
@@ -0,0 +1,26 @@
+/*  
+  Dateiname     : Warenautomat.java.
+  Veranstaltung : Anwendungsprogrammierung (BIS-201).
+  Fragestellung : Rahmen Warenautomat.
+  Version       : 09.10.2020
+*/
+//Bitte entsprechend anpassen:
+package Uebung_02;
+
+public class Warenautomat
+{
+            
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
