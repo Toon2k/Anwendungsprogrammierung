6ffedffa0429c42f910903e8b7f6ae21c065a24b
Toon2k <janiswelsch@gmail.com>
2025-08-04 20:22:31 +0200

    Übung 01 Ansatz

--- src/Uebung_01/Sinustabelle.java
+++ src/Uebung_01/Sinustabelle.java
@@ -10,7 +10,24 @@ public class  Sinustabelle {
 
   public static void main(String [] argumente)
   {
-     
+      // For Schleife 1-40 * pi/20 in array1 füllen
+      // For Schleife 1-40 sin(array1(i)) in array2 füllen
+
+
+      // print auch als For Schleife? (erste Linie so, dann immer Zeile + untere Linie in Schleife)
+      System.out.println("-------------------");
+      System.out.println("|   x    | sin(x) |");
+      System.out.println("-------------------");
+      System.out.println("|        |        |");
+      System.out.println("-------------------");
+      System.out.println("|        |        |");
+      System.out.println("-------------------");
+      System.out.println("|        |        |");
+      System.out.println("-------------------");
+      System.out.println("|        |        |");
+      System.out.println("-------------------");
+      System.out.println("|        |        |");
+      System.out.println("-------------------");
   }
 
 }
