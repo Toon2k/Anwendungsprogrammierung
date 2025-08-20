ec1501c5833084363ca23998f464c1bf0a3edd2c
Toon2k <janiswelsch@gmail.com>
2025-08-20 10:59:31 +0200

    Übung 01

--- src/Uebung_01/Ostersonntag.java
+++ src/Uebung_01/Ostersonntag.java
@@ -23,10 +23,36 @@ public class Ostersonntag
   public static void ermittleOstersonntag(int jahr)
   {
   int tag = 0, monat = 0;
-  
-  // Hier die Beschreibung von Gauß (den Algorithmus) in
-  // Java-Anweisungen übertragen.
-  
+  int rest19 = jahr % 19;
+  int rest4 = jahr % 4;
+  int rest7 = jahr % 7;
+  int pruefziffer, a6, a7, a8, a9;
+
+  if (jahr >= 1900 && jahr <= 2099) {
+    pruefziffer = 5;
+  } else {
+    pruefziffer = 6;
+  }
+
+  a6 = (19 * rest19 + 24) % 30;
+  a7 = (2 * rest4 + 4 * rest7 + 6 * a6 + pruefziffer) % 7;
+  a8 = 22 + a6 + a7;
+  a9 = a6 + a7 -9;
+
+  if (a8 >= 1 && a8 <= 31) {
+    tag = a8;
+    monat = 3;
+  } else if (a9 == 26) {
+      tag = 19;
+      monat = 4;
+  } else if (a9 == 25 && a6 == 28 && rest19 > 10) {
+      tag = 18;
+      monat = 4;
+  } else {
+      tag = a9;
+      monat = 4;
+  }
+
       System.out.format("Ostersonntag im Jahr %4d am %02d.%02d%n", 
                          jahr, tag, monat);
   }
--- src/Uebung_01/Sinustabelle.java
+++ src/Uebung_01/Sinustabelle.java
@@ -8,26 +8,38 @@ package Uebung_01;
 
 public class  Sinustabelle {
 
-  public static void main(String [] argumente)
+  public static void main(String [] args)
   {
+      double[] xWerte = new double[40];
+      int i = 0;
+      double x = 0;
+      double[] sinWerte = new double[40];
+
       // For Schleife 1-40 * pi/20 in array1 füllen
+      for (i = 0; i <= xWerte.length-1; i++) {
+          x += Math.PI/20;
+          xWerte[i] = x;
+      }
+
       // For Schleife 1-40 sin(array1(i)) in array2 füllen
+      for (i = 0; i<= xWerte.length-1; i++) {
+          sinWerte[i] = Math.sin(xWerte[i]);
+      }
 
-
-      // print auch als For Schleife? (erste Linie so, dann immer Zeile + untere Linie in Schleife)
-      System.out.println("-------------------");
+      System.out.println("--------------------");
       System.out.println("|   x    | sin(x) |");
-      System.out.println("-------------------");
-      System.out.println("|        |        |");
-      System.out.println("-------------------");
-      System.out.println("|        |        |");
-      System.out.println("-------------------");
-      System.out.println("|        |        |");
-      System.out.println("-------------------");
-      System.out.println("|        |        |");
-      System.out.println("-------------------");
-      System.out.println("|        |        |");
-      System.out.println("-------------------");
+      System.out.println("--------------------");
+      for (i=0; i< xWerte.length; i++) {
+          if (sinWerte[i] < 0) {
+              System.out.printf("| %.4f | %.4f |%n", xWerte[i], sinWerte[i]);
+              System.out.println("--------------------");
+          }
+          else {
+              System.out.printf("| %.4f | %.4f  |%n", xWerte[i], sinWerte[i]);
+              System.out.println("--------------------");
+          }
+      }
+
   }
 
 }
